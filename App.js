f2485ff7174d98ce69a034d4d6b631b79e1e1ff8
Ibrhaim Shaker <shaker.personnel@gmail.com>
2020-11-23 16:02:48 +0200

    Implement Modal

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 
 
@@ -17,10 +17,24 @@ import ListingsScreen from "./app/screens/ListingsScreen";
 import AppTextInput from './app/components/AppTextInput';
 import AppPicker from './app/components/AppPicker';
 
+const categories = [
+  {label: "Furniture", value: 1},
+  {label: "Clothing", value: 2},
+  {label: "Cameras", value: 3}
+]
+
 export default function App() {
+
+  const [category, setCategory] = useState(categories[0]);
+
   return (
     <Screen>
-      <AppPicker iconName="apps" placeholder="Category" />
+      <AppPicker 
+      selectedItem={category}
+      onSelectItem={item => setCategory(item)}
+      items={categories} 
+      iconName="apps" 
+      placeholder="Category" />
       <AppTextInput iconName="email" placeholder="Email"/>
     </Screen>
   );
