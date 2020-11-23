01dea66d2457a9f5f10af8660e31daa4fe876637
Ibrhaim Shaker <shaker.personnel@gmail.com>
2020-11-23 04:51:40 +0200

    Icon component

--- App.js
+++ App.js
@@ -9,10 +9,14 @@ import colors from './app/config/colors';
 import Card from "./app/components/Card";
 import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 import MessagesScreen from './app/screens/MessagesScreen';
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
   return (
-    <MessagesScreen />
+    <Screen>
+      <Icon name="email" size={40} backgroundColor="red" iconColor="white" />
+    </Screen>
   );
 }
 
