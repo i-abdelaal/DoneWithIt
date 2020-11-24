a6cf1492f78e02f08f1e50fbd8b30b265b1be283
Ibrhaim Shaker <shaker.personnel@gmail.com>
2020-11-24 03:17:43 +0200

    Index lists and reimport from it

---
+++ app/screens/ListingEditScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+function ListingEditScreen(props) {
+  return <div></div>;
+}
+
+const styles = StyleSheet.create({});
+
+export default ListingEditScreen;
