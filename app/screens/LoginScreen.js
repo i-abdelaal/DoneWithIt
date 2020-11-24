5afb86e9c95689b5bf2612f68d40ce2b920f244b
Ibrhaim Shaker <shaker.personnel@gmail.com>
2020-11-24 03:05:35 +0200

    Index the form and import from it

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -4,9 +4,7 @@ import { Image, StyleSheet } from "react-native";
 import * as Yup from "yup";
 
 import Screen from "../components/Screen";
-import AppFormField from "../components/forms/AppFormField";
-import SubmitButton from "../components/forms/SubmitButton";
-import AppForm from "../components/forms/AppForm";
+import { AppForm, AppFormField, SubmitButton } from "../components/forms";
 
 const validationSchema = Yup.object().shape({
   email: Yup.string().required().email().label("Email"),
